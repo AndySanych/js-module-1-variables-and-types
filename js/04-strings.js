cf1f9860c8e1d93afaf1b90ce6580a919203c3ba
AndreyMachula <amcz2020@gmail.com>
2023-08-25 12:08:07 +0200

    modifies the script

--- js/04-strings.js
+++ js/04-strings.js
@@ -52,12 +52,33 @@ const orderMsg = `Вы заказываете ${quantity} холодильник
 
 // console.log(normalizedBrand)
 
-// Вариант 1
+// Вариант 2
 let brand = 'Samsung';
 brand = brand.toLowerCase();
 
-console.log(brand)
+// console.log(brand);
+// console.log(brand.length - 1); // длинна символов
+// console.log(brand[6]); // поиск по индексу
 
 /*
  * Поиск в строке с методом includes()
  */
+
+const blacklistedWord1 = 'спам';
+const blacklistedWord2 = 'распродажа';
+
+const string1 = 'Привет, я принц Абдул, это не спам, предлагаю тебе миллион!';
+const string2 = 'Самая большая РАСПРОДАЖА этой недели, не пропустите!';
+const string3 = 'Рекламная кампания #fatlivesmatter';
+
+// console.log(string1.includes(blacklistedWord1));
+// console.log(string1.includes(blacklistedWord2));
+
+// console.log(string2.includes(blacklistedWord1));
+// console.log(string2.toLowerCase().includes(blacklistedWord2));
+
+// const normalizedString2 = string2.toLowerCase();
+// console.log(normalizedString2.includes(blacklistedWord2));
+
+// console.log(string3.includes(blacklistedWord1));
+// console.log(string3.includes(blacklistedWord2));
